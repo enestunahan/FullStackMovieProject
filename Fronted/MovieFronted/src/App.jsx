b3985021f3d53efda3c3f67df907f220c9b836a0
Enes Tunahan <enesstunahan@gmail.com>
2024-06-05 22:22:55 +0300

    components has been created

--- Fronted/MovieFronted/src/App.jsx
+++ Fronted/MovieFronted/src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Movies from './Components/Movies/Movies'
 
 function App() {
 
   return (
     <>
      
+     <Navbar/>
+
+     <Home/>
+     <Movies/>
+
+     
+     <Footer/>
+     
+
     </>
   )
 }
---
+++ Fronted/MovieFronted/src/Components/Movies/Movies.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Movies.css'
+
+export default function Movies() {
+  return (
+    <div>Movies</div>
+  )
+}
