fea7aa96b80bb93f73d22e4deaa4cd349b1613ce
Enes Tunahan <enesstunahan@gmail.com>
2024-06-06 20:13:12 +0300

    react-router-dom paketi indirildi , routing mekanizması hazırlandı

--- Fronted/MovieFronted/src/App.jsx
+++ Fronted/MovieFronted/src/App.jsx
@@ -1,23 +1,55 @@
 import { useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Movies from './Components/Movies/Movies'
+import {Routes , Route, useLocation} from 'react-router-dom'
+import Home from './Components/Home/Home'
+import Footer from './Components/Footer/Footer'
+import Admin from './Components/Admin/Admin'
+import Login from './Components/Login/Login'
 
 function App() {
+
+
+  const location = useLocation();
+  const showNavbarAndFooter = location.pathname !== '/admin';
+  const[isAuthenticated , setIsAuthenticated] = useState(false);
 
   return (
     <>
      
-     <Navbar/>
+    {showNavbarAndFooter && <Navbar/>}
 
-     <Home/>
-     <Movies/>
-
+     <Routes>
+          <Route path = ''        element =  {<Home/>}/>
+          <Route path = '/movies' element =  {<Movies/>}/>
+          <Route path = '/admin'  element =  {<Admin/>}/>
+          {/* <Route path='/login'>
+              <Login setIsAuthenticated = {setIsAuthenticated} />
+          </Route>
+          <PrivateRoute path= '/admin' element={<Admin/>} isAuthenticated={isAuthenticated}/> */}
+          <Route path='/admin' element = {<Admin/>} />
+     </Routes>
      
-     <Footer/>
+
+    {showNavbarAndFooter && <Footer/>}  
      
 
     </>
   )
 }
+
+const PrivateRoute = ({ element: Component, isAuthenticated, ...rest }) => (
+  <Route
+      {...rest}
+      render={(props) =>
+          isAuthenticated ? (
+              <Component {...props} />
+          ) : (
+              <Redirect to="/login" />
+          )
+      }
+  />
+);
+
 
 export default App
--- Fronted/MovieFronted/src/Components/Login/Login.jsx
+++ Fronted/MovieFronted/src/Components/Login/Login.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
+// import {useHistory} from 'react-router-dom'
 
-export default function Login() {
-  return (
-    <div>Login</div>
-  )
+
+export default function Login({setIsAuthenticated}) {
+  
+  const[userName, setUserName] = useState('');
+  const[password, setPassword] = useState('');
+  //const history = useHistory();
+  
+  const handleSubmit = (e) => {
+
+    e.preventDefault();
+    
+    if(userName == 'admin' && password === '1234'){
+        
+        setIsAuthenticated(true);
+       // history.push('/admin');
+    
+    } else{
+        alert('kullanıcı adı veya şife hatalı');
+    }
+
+  }
+  
+    return (
+
+        <div className='login'>
+            <form onSubmit={handleSubmit}>
+
+                <button type='submit'></button>
+            </form>
+        </div>
+  
+)
 }
